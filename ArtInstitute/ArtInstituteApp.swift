f836dad21d3ea34e531b0acbb2d3c3e89378aeaf
Syed Fahad <fahadrasheedsyed@gmail.com>
2026-06-18 18:59:01 -0400

    Initial Commit

---
+++ ArtInstitute/ArtInstituteApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtInstituteApp.swift
+//  ArtInstitute
+//
+//  Created by Syed Fahad Rasheed on 2026-06-18.
+//
+
+import SwiftUI
+
+@main
+struct ArtInstituteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
